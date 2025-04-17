6bb2d6ef03d305de908c1f515f32dcee2ad21f33
Charvi <charvikapoor1996@gmail.com>
2025-04-17 20:18:26 +0530

    Video 57 complete

---
+++ Video 57/index.js
@@ -0,0 +1,33 @@
+console.log("I am a tutorial on loops")
+// let a = 1;
+// for (let i = 0; i < 100; i++) {
+//     console.log(i);
+    
+// }
+
+// let obj = {
+//     "name": "Andy",
+//     "role": "programmer",
+//     "company": "codewithharry AI"
+// }
+
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(key)
+//     }
+
+// for (const c of [1, 2, 3]) {
+//     console.log(c);
+// }
+
+// let i = 1;
+// while(i < 6){
+//     console.log(i);
+//     i++;
+// }
+
+let i = 10;
+do {
+    console.log(i);
+    i++;
+} while (i < 6);
