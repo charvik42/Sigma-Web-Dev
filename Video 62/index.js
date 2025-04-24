64f0b74b13416947574aef566c85fb53d3c3f397
Charvi <charvikapoor1996@gmail.com>
2025-04-24 20:18:14 +0530

    Video 64 complete

---
+++ Video 62/index.js
@@ -0,0 +1,14 @@
+/* Adjectives:
+Crazy
+Amazing
+Fire
+
+Shop Name:
+Engine
+Foods
+Garments
+
+Another word:
+Bros
+Limited
+Hub */
---
+++ Video 63/index.js
@@ -0,0 +1,36 @@
+// let arr = [1, 2, 4, 5, 7]
+// // console.log(arr);
+// // console.log(arr.length)
+// arr[0] = 5666;
+// // console.log(arr[0], arr[1], arr[2], arr[3], arr[4])
+// // console.log(typeof(arr))
+
+// console.log(arr.toString())
+// console.log(arr.join(" and "))
+
+// a = [1, 2, 3, 4, 5, 6]
+// // console.log(a.pop())
+// // console.log(a)
+// a.push(100)
+// console.log(a)
+// // a.push("new")
+// // console.log(a)
+// console.log(a.shift())
+// console.log(a.unshift("New"))
+// console.log(a)
+// delete a[5]
+// console.log(a)
+
+// let a1 = [1, 2, 3]
+// let a2 = [4, 5, 6]
+// let a3 = [9, 8, 7]
+
+// console.log(a1.concat(a2, a3))
+// console.log(a3.sort())
+
+let numbers = [1, 2, 3, 4, 5]
+// numbers.splice(1, 3)
+// console.log(numbers)
+// numbers.splice(1, 3, 222, 333)
+console.log(numbers.slice(2))
+console.log(numbers.reverse())
